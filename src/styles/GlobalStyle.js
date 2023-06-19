d30b4fb58c2a44b42ff8e2d08654100c8cb157a9
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-06-19 16:56:40 +0200

    Temporary select options, css

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -9,22 +9,63 @@ export default createGlobalStyle`
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    /* text-align: center; */
     color: #2c3e50;
   }
 
   body, #root {
-    margin: 0;
+    /* margin: 0;
     display: flex;
     flex-direction: column;
-    min-height: 100vh;
+    min-height: 100vh; */
   }
 
   .main {
-    flex: 1;
+    /* flex: 1; */
+  }
+  
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
-  .bg-dark {
-    background-color: #12002b;
+  .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+  }
+
+  label {
+      display: block;
+      margin-top: 1rem;
+      margin-bottom: 10px;
+  }
+
+  .address {
+      margin-top: 10px;
+  }
+
+  select {
+    &#state, &#department {
+      border: 1px solid #c5c5c5;
+      border-radius: 3px;
+      padding: 0.4em 1em;
+      background: #f6f6f6;
+      color: #454545;
+    }
+  }
+
+  #create-employee {
+    width: 300px;
+
+    input {
+      /* width: 100%; */
+    }
+  }
+
+  #confirmation {
+    margin-top: 1rem;
   }
 `
