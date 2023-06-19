ccc6e1578052cd67bd34550fc4dff09ac91e0878
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-06-19 16:02:15 +0200

    Adding Link components

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+
 export const Employees = () => {
   return (
-    <div>Employees</div>
+    <body>
+      <div id="employee-div" class="container">
+        <h1>Current Employees</h1>
+        <table id="employee-table" class="display"></table>
+        <Link to='/'>Home</Link>
+      </div>
+    </body>
   )
 }
