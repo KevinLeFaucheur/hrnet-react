9ce6b5240de3aeae4deec0f6ae7c766738f82365
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-07-06 15:53:48 +0200

    Space and Enter select option, escape returns

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -6,7 +6,6 @@ import { Icon } from "./assets/Icon";
  * TODO: 
  * - CSS options
  * - accessibility:
- *    - space or enter should select a value
  *    - arrow up and down on input should shuffle options too
  * - more tolerant options building (optional)
  * - upwards is broken
@@ -34,7 +33,7 @@ export const Select = ({ placeHolder, options, onChange }) => {
 
     window.addEventListener('click', handler);
     return () => { window.removeEventListener('click', handler) }
-  }, [focus, keyCode, showMenu])
+  }, [focus, showMenu])
 
   const setPlaceholder = () => {
     return selectedValue ? selectedValue.label : placeHolder ? placeHolder : options[0].label;
@@ -50,18 +49,28 @@ export const Select = ({ placeHolder, options, onChange }) => {
     e.preventDefault();
     if(e.keyCode === 32) setShowMenu(!showMenu);
     setKeyCode(e.keyCode);
+
+    let index = focus;
+    switch(e.keyCode) {
+      case 38: if(index > 0) index--; break;
+      case 40: if(index < options.children.length-1) index++; break;
+      default:
+    }
+
     setFocus(0);
   }
 
-  const handleOption = (e) => {
+  const handleOption = (option, e) => {
     e.stopPropagation();
     e.preventDefault();
-    if(e.keyCode === 32) setShowMenu(!showMenu);
+    if([13, 27, 32].includes(e.keyCode)) setShowMenu(!showMenu);
     setKeyCode(e.keyCode);
 
     let selectMenu = document.querySelector('.select-menu');
     let index = focus;
     switch(e.keyCode) {
+      case 13:
+      case 32: setSelectedValue(option); break;
       case 38: if(index > 0) index--; break;
       case 40: if(index < selectMenu.children.length-1) index++; break;
       default:
@@ -96,7 +105,7 @@ export const Select = ({ placeHolder, options, onChange }) => {
         {options.map(option => (
           <div 
             tabIndex={0}
-            onKeyDown={handleOption}
+            onKeyDown={(e) => handleOption(option, e)}
             onClick={() => onItemClick(option)} 
             key={option.value} 
             className={`select-item ${isSelected(option) && "selected"}`}>
